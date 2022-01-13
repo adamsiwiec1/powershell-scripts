0b116f48f187194f844e13892534c39605c7499d
Adam Siwiec <73085927+adamsiwiec1@users.noreply.github.com>
2022-01-13 12:58:13 -0500

    Rename automation/email.ps1 to _SCRIPTS/email.ps1

--- _SCRIPTS/email.ps1
+++ _SCRIPTS/email.ps1
@@ -8,4 +8,4 @@ $secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
 $botcreds = New-Object System.Management.Automation.PSCredential ($bot, $secpasswd) 
 
 
-Send-MailMessage -To "adam2.siwiec@gmail.com" -From "emailbot.ps1@gmail.com" -Subject "Sent using powershell" -Body "This is an important message" -Credential "emailbot.ps1@gmail.com" -SmtpServer "smtp.gmail.com" -Port 25 -UseSsl -Force
+Send-MailMessage -To "adam2.siwiec@gmail.com" -From "emailbot.ps1@gmail.com"Â -Subject "Sent using powershell" -Body "This is an important message"Â -Credential "emailbot.ps1@gmail.com" -SmtpServer "smtp.gmail.com"Â -Port 25 -UseSsl -Force
